bce00e7036070959680a8a92d739cf671878f6a3
Andrei Milosoiu <andrei18.milosoiu@gmail.com>
2026-02-21 14:55:46 +0200

    Removed startup feature for now

--- gui.py
+++ gui.py
@@ -24,12 +24,6 @@ def click_update_button():
 def display(x):
 	selection = x
 
-def on_button_toggle():
-    if run_start.get() == 1:
-        print("Checkbutton is selected")
-    else:
-        print("Checkbutton is deselected")
-
 # Main
 root = tk.Tk(
 	screenName=None,
@@ -106,19 +100,7 @@ update_button = ttk.Button(
 	width=25,
 	command=click_update_button
 )
-update_button.pack(padx=20, pady=20)
-
-# Checkbutton (startup)
-run_start = tk.IntVar()
-checkbutton = ttk.Checkbutton(
-	root,
-	text="Run on startup with current settings",
-	variable=run_start,
-	onvalue=1,
-	offvalue=0,
-	command=on_button_toggle
-)
-checkbutton.pack(
+update_button.pack(
 	padx=20,
 	pady=20
 )
