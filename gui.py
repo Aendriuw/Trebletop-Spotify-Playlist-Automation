9edf58f6ad2bf375f3f2f222f9963ae5d3972269
Andrei Milosoiu <andrei18.milosoiu@gmail.com>
2026-02-21 14:06:22 +0200

    Added functional combobox for time selection

--- gui.py
+++ gui.py
@@ -8,57 +8,101 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 
+# Functions
 def click_update_button():
-	command1 = "spotify-script.py"
-	cmd_song_cnt = current_value.get()
+	cmd_script = "spotify-script.py"
+	cmd_song_cnt = curr_song_cnt.get()
+	aux_term = curr_term.get()
 	cmd_term = "short_term"
+	if aux_term == "Last 6 months":
+		cmd_term = "medium_term"
+	elif aux_term == "All time":
+		cmd_term = "long_term"
 
-	subprocess.call(['python3', command1, cmd_song_cnt, cmd_term])
+	subprocess.call(['python3', cmd_script, cmd_song_cnt, cmd_term])
 
-def display():
-	selection = song_cnt.get()
+	print()
 
-root = tk.Tk(screenName=None, baseName=None, className='Tk', useTk=1)
+def display(x):
+	selection = x
+
+# Main
+root = tk.Tk(
+	screenName=None,
+	baseName=None,
+	className='Tk',
+	useTk=1
+)
 root.title("TrebleTop")
 root.geometry("500x300")
 
 # TrebleTop Name
-label = ttk.Label(root, text="TrebleTop")
+label = ttk.Label(
+	root,
+	text="TrebleTop"
+)
 label.pack()
 
 # Spinbox
 # 1. Spinbox Label
-label = ttk.Label(root, text="How many songs in the playlist?")
+label = ttk.Label(
+	root,
+	text="Number of songs in the playlist:"
+)
 label.pack()
 
 # 2. Spinbox Body
-current_value = tk.StringVar(value=100)
+curr_song_cnt = tk.StringVar(value=100)
 song_cnt = ttk.Spinbox(
 	root,
 	from_=50,
 	to=1000,
 	increment = 50,
-	command=display,
-	textvariable=current_value)
+	command=display(curr_song_cnt.get()),
+	textvariable=curr_song_cnt
+)
 
-song_cnt.config(
-	width=30)
+song_cnt.config(width=30)
 
-song_cnt.pack(padx=20, pady=20)
+song_cnt.pack(
+	padx=20,
+	pady=20
+)
 
-# Combobox: term
+# Combobox
+# 1. Combobox Label
+label = ttk.Label(
+	root,
+	text="Time interval:")
+label.pack()
+
+# 2. Combobox Body
+curr_term = tk.StringVar(value=100)
 term = ttk.Combobox(
     root,
-    values=["short_term", "medium_term", "long_term"],
-    state="readonly"
+    values=["Last month", "Last 6 months", "All time"],
+    state="readonly",
+	# command=display(curr_term.get()),
+	textvariable=curr_term
 )
 term.pack(pady=5)
-term.set("short_term")
-term.bind("<<ComboboxSelected>>", select)
-term.config(justify="center", width=30)
+term.set("Last month")
+term.bind(
+	"<<ComboboxSelected>>",
+	select
+)
+term.config(
+	justify="center",
+	width=30
+)
 
 # Update Button
-update_button = ttk.Button(root, text="Update Playlist", width=25, command=click_update_button)
-update_button.pack()
+update_button = ttk.Button(
+	root,
+	text="Update Playlist",
+	width=25,
+	command=click_update_button
+)
+update_button.pack(padx=20, pady=20)
 
 root.mainloop()
