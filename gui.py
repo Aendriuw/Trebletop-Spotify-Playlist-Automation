a540f83e99e8921fb4cb13585221e5bc2314e7b8
Andrei Milosoiu <andrei18.milosoiu@gmail.com>
2026-02-21 13:44:22 +0200

    Added Spinbox for song count.

--- gui.py
+++ gui.py
@@ -1,18 +1,64 @@
 # Milosoiu Andrei
 # Email: andrei18.milosoiu@gmail.com
 
-import tkinter as tk
 import os
+import subprocess
+import select
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
 def click_update_button():
-	os.system("python spotify-script.py 100 short_term")
+	command1 = "spotify-script.py"
+	cmd_song_cnt = current_value.get()
+	cmd_term = "short_term"
+
+	subprocess.call(['python3', command1, cmd_song_cnt, cmd_term])
+
+def display():
+	selection = song_cnt.get()
 
 root = tk.Tk(screenName=None, baseName=None, className='Tk', useTk=1)
+root.title("TrebleTop")
+root.geometry("500x300")
 
-label = tk.Label(root, text="TrebleTop")
+# TrebleTop Name
+label = ttk.Label(root, text="TrebleTop")
 label.pack()
 
-update_button = tk.Button(root, text="Update Playlist", width=25, command=click_update_button)
+# Spinbox
+# 1. Spinbox Label
+label = ttk.Label(root, text="How many songs in the playlist?")
+label.pack()
+
+# 2. Spinbox Body
+current_value = tk.StringVar(value=100)
+song_cnt = ttk.Spinbox(
+	root,
+	from_=50,
+	to=1000,
+	increment = 50,
+	command=display,
+	textvariable=current_value)
+
+song_cnt.config(
+	width=30)
+
+song_cnt.pack(padx=20, pady=20)
+
+# Combobox: term
+term = ttk.Combobox(
+    root,
+    values=["short_term", "medium_term", "long_term"],
+    state="readonly"
+)
+term.pack(pady=5)
+term.set("short_term")
+term.bind("<<ComboboxSelected>>", select)
+term.config(justify="center", width=30)
+
+# Update Button
+update_button = ttk.Button(root, text="Update Playlist", width=25, command=click_update_button)
 update_button.pack()
 
 root.mainloop()
